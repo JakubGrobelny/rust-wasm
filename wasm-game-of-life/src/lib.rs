6fb8c5b6456948876c9e89c59021dc8b513b2907
Jakub Grobelny <jgrobelny1@gmail.com>
2020-01-11 22:07:33 +0100

    tutorial: optimized storage

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+extern crate js_sys;
+extern crate fixedbitset;
+use fixedbitset::FixedBitSet;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -20,7 +23,7 @@ pub enum Cell {
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>
+    cells: FixedBitSet,
 }
 
 #[wasm_bindgen]
@@ -29,25 +32,18 @@ impl Universe {
         let width = 64;
         let height = 64;
 
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, js_sys::Math::random() < 0.5);
+        }
 
         Universe {
             width,
             height,
             cells,
         }
-    }
-
-    pub fn render(&self) -> String {
-        self.to_string()
     }
 
     pub fn width(&self) -> u32 {
@@ -58,8 +54,8 @@ impl Universe {
         self.height
     }
 
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
     }
 
     fn get_index(&self, row: u32, col: u32) -> usize {
@@ -93,32 +89,15 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbours = self.live_neighbour_count(row, col);
 
-                let next_cell = match (cell, live_neighbours) {
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    (Cell::Alive, x) if x < 2 || x > 3  => Cell::Dead,
-                    (Cell::Dead, 3)                     => Cell::Alive,
-                    (state, _)                          => state,
-                };
-
-                next[idx] = next_cell;
+                next.set(idx, match (cell, live_neighbours) {
+                    (true, 2) | (true, 3) => true,
+                    (true, x) if x < 2 || x > 3 => false,
+                    (false, 3) => true,
+                    (state, _) => state,
+                });
             }
         }
 
         self.cells = next;
-    }
-}
-
-use std::fmt;
-
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
-        Ok(())
     }
 }
