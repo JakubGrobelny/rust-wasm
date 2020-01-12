94c072bcb76af4b81b1fa93b4719b0e6166fbf03
Jakub Grobelny <jgrobelny1@gmail.com>
2020-01-12 02:20:08 +0100

    fractal: solution skeleton

--- fractal/src/lib.rs
+++ fractal/src/lib.rs
@@ -13,4 +13,93 @@ extern {
     fn alert(s: &str);
 }
 
+#[wasm_bindgen]
+struct AnimationState {
+    shift: f64,
+    zoom: f64,
+    size: usize,
+    data: *const u8,
+    up_to_date: bool
+}
+
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8
+}
+
+const R : usize = 0;
+const G : usize = 1;
+const B : usize = 2;
+const A : usize = 3;
+const COLOR_SIZE : usize = 4;
+
+fn find_point_color(x: f64, y: f64) -> Color {
+    // TODO:
+    Color {r: 64, g: 32, b: 128}
+}
+
+fn rescale(point: usize, shift: f64, zoom: f64) -> f64 {
+    // TODO:
+    point as f64
+}
+
+fn translate_coordinates(row: usize, col: usize, size: usize) -> usize {
+    (row * size + col) * COLOR_SIZE
+}
+
+fn generate_picture(shift: f64, zoom: f64, size: usize) -> *const u8 {
+    let mut data : Vec<u8> = vec![255; size * size * 4];
+
+    for row in 0..size {
+        for col in 0..size {
+            let y = rescale(row, shift, zoom);
+            let x = rescale(col, shift, zoom);
+            let index = translate_coordinates(row, col, size);
+            let color = find_point_color(x, y);
+            data[index + R] = color.r;
+            data[index + G] = color.g;
+            data[index + B] = color.b;
+        }
+    }
+
+    data.as_ptr()
+}
+
+#[wasm_bindgen]
+impl AnimationState {
+    pub fn new(shift: f64, zoom: f64, size: usize) -> AnimationState {
+        AnimationState {
+            shift: shift, 
+            zoom: zoom,
+            size: size,
+            data: generate_picture(shift, zoom, size),
+            up_to_date: true
+        }
+    }
+
+    pub fn get_data(&mut self) -> *const u8 {
+        if !self.up_to_date {
+            self.data = generate_picture(
+                self.shift,
+                self.zoom,
+                self.size
+            );
+            self.up_to_date = true;
+        }
+
+        self.data
+    }
+
+    pub fn zoom(&mut self, amount: f64) {
+        self.zoom += amount;
+        self.up_to_date = false;
+    }
+
+    pub fn shift(&mut self, amount: f64) {
+        self.shift += amount;
+        self.up_to_date = false;
+    }
+}
+
 
--- fractal/www/index.js
+++ fractal/www/index.js
@@ -1,3 +1,26 @@
-import * as wasm from "fractal";
+import * as fractal from "fractal";
+import { memory } from "fractal/fractal_bg"
+import { FractalAnimation } from "fractal";
 
-canvas = document.getElementById("fractal-canvas");
+let canvas = document.getElementById("fractal-canvas");
+
+const size = 1024;
+canvas.height = size;
+canvas.width = size;
+
+const context = canvas.getContext('2d');
+
+const animation = fractal.AnimationState.new(0.0, 1.0, size);
+
+const renderLoop = () => {
+    let pixels = new Uint8ClampedArray(
+        memory.buffer,
+        animation.get_data(),
+        size * size * 4
+    );
+    let imageData = new ImageData(pixels, size, size);
+    context.putImageData(imageData, 0, 0);
+    requestAnimationFrame(renderLoop);
+};
+
+renderLoop();
